e71ef77a4e65d99ef3e9d63f6a673a53569c1880
taiyangzhishen <https://github.com/taiyangzhishen/guanzhichuangzhaoshen.git>
2016-12-11 03:20:23 +0800

    0

---
+++ App2/App2/pic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+namespace App2
+{
+    class pic : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return 0;
+        }
+        public object ConvertBack(object value,Type targetType,object parameter,string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
